7a50981498efaedc3d628cef4c533a5797d19cef
Zsombor Welker <flaktack@welker.hu>
2013-03-26 06:26:06 +0100

    util/gtfs-shapes.pl: Refactor to work with more GTFS feeds

--- util/gtfs-shapes.pl
+++ util/gtfs-shapes.pl
@@ -23,38 +23,49 @@ use strict;
 use warnings;
 use 5.12.0;
 
+use Data::Dumper;
+use Text::CSV::Encoded;
+
+my $CSV = Text::CSV::Encoded->new(
+	{
+		encoding_in  => 'utf8',
+		encoding_out => 'utf8',
+		sep_char     => ',',
+		quote_char   => '"',
+		escape_char  => '"',
+	}
+);
+
 my $shapes = {};
 my $ways   = [];
 my $way_l  = {};
 my $nodes  = {};
 my $node   = -100000;
 
-my $dir = $ARGV[0] || 'feeds/bkv/gtfs';
+my $DIR = $ARGV[0] || 'feeds/bkv/gtfs';
 
 my ( $file, $routes, $lines );
 
-open( $file, "$dir/routes.txt" );
-binmode( $file, ':utf8' );
-<$file>;
-for (<$file>) {
-	my ( $id, $route, $name, $type ) = ( $_ =~ m/^(.*?),.*?,(.*?),.*?,"(.*?)",(\d)/ );
-	$routes->{$id} = $route || $name;
+sub remove_bom(@);
+
+open( $file, "$DIR/routes.txt" );
+$CSV->column_names( remove_bom $CSV->getline($file) );
+while ( my $cols = $CSV->getline_hr($file) ) {
+	$routes->{ $cols->{route_id} } = $cols->{route_short_name} || $cols->{route_long_name};
 }
 
-open( $file, "$dir/trips.txt" );
-binmode( $file, ':utf8' );
-<$file>;
-for (<$file>) {
-	chomp;
-	my ( $route, $shape ) = ( $_ =~ m/^([^,]*),.*?,([^,]*)$/ );
-	$lines->{$shape}->{ $routes->{$route} } = 1;
+open( $file, "$DIR/trips.txt" );
+$CSV->column_names( remove_bom $CSV->getline($file) );
+while ( my $cols = $CSV->getline_hr($file) ) {
+	$lines->{ $cols->{shape_id} }->{ $routes->{ $cols->{route_id} } } = 1;
 }
 
-open( $file, "$dir/shapes.txt" );
-binmode( $file, ':utf8' );
-<$file>;
-while (<$file>) {
-	my ( $id, $lat, $lon, $seq, $dist ) = ( split ',', $_ );
+open( $file, "$DIR/shapes.txt" );
+$CSV->column_names( remove_bom $CSV->getline($file) );
+while ( my $cols = $CSV->getline_hr($file) ) {
+	my ( $id, $lat, $lon, $seq, $dist )
+		= ( map { $cols->{$_} }
+			qw/shape_id shape_pt_lat shape_pt_lon shape_pt_sequence shape_dist_traveled/ );
 	push @{ $shapes->{$id} }, [ $seq, $lat, $lon ];
 }
 
@@ -66,20 +77,21 @@ EOF
 for ( keys %$shapes ) {
 	my @way = ($_);
 	for ( sort { $a->[0] <=> $b->[0] } @{ $shapes->{$_} } ) {
-		if ( $nodes->{ $_->[1] . "-" . $_->[2] } ) {
-			push @way, $nodes->{ $_->[1] . "-" . $_->[2] };
+		my $nkey = $_->[1] . "-" . $_->[2];
+		if ( $nodes->{$nkey} ) {
+			push @way, $nodes->{$nkey};
 		}
 		else {
 			push @way, $node;
 			print <<EOF;
 	<node id="$node" lat="$_->[1]" lon="$_->[2]" />
 EOF
-			$nodes->{ $_->[1] . "-" . $_->[2] } = $node;
+			$nodes->{$nkey} = $node;
 			$node--;
 		}
 	}
 
-	my $w = join ",", @way[1...$#way];
+	my $w = join ",", @way[ 1 ... $#way ];
 	if ( $way_l->{$w} ) {
 		$way_l->{$w}->[0] .= ', ' . $way[0];
 		$lines->{ $way_l->{$w}[0] }->{$_} = 1 for ( keys %{ $lines->{ $way[0] } } );
@@ -115,3 +127,9 @@ EOF
 print <<EOF;
 </osm>
 EOF
+
+sub remove_bom(@)
+{
+	return [ map { s/^\x{feff}//; $_ } @{ $_[0] } ];
+}
+
