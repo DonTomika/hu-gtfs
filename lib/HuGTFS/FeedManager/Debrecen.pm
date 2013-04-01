449e18938eb1cacfd6fd09e760e6a9af718a0995
Zsombor Welker <flaktack@welker.hu>
2013-04-01 11:49:00 +0200

    Add Debrecen FM for customizing the agency_id

---
+++ lib/HuGTFS/FeedManager/Debrecen.pm
@@ -0,0 +1,55 @@
+=head1 NAME
+
+HuGTFS::FeedManager::Debrecen - HuGTFS feed manager for download + merging existing Debrecen GTFS data with OSM
+
+=head1 SYNOPSIS
+
+	use HuGTFS::FeedManager::Debrecen;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+package HuGTFS::FeedManager::Debrecen;
+
+use 5.14.0;
+use utf8;
+use strict;
+use warnings;
+
+use Mouse;
+
+extends 'HuGTFS::FeedManager::GTFS';
+__PACKAGE__->meta->make_immutable;
+
+my $log = Log::Log4perl->get_logger(__PACKAGE__);
+
+my $agencies = {
+	'HV_DB' => 'HAJDU-VOLAN-DEBRECEN',
+	'KT_DB' => 'KOMAROMI-TARSA-DEBRECEN',
+	'TESCO' => 'TESCO-DEBRECEN',
+};
+
+after 'fixup_agency' => sub {
+	my ($self, $agency) = @_;
+	$agency->{agency_id} =  $agencies->{$agency->{agency_id}} if $agencies->{$agency->{agency_id}};
+};
+
+after 'fixup_route' => sub {
+	my ($self, $route) = @_;
+
+	$route->{agency_id} = $agencies->{$route->{agency_id}} if $agencies->{$route->{agency_id}};
+	$route->{route_text_color} = '000000' if defined $route->{route_text_color} && $route->{route_text_color} eq '0';
+};
+
+1;
+
+=head1 COPYRIGHT
+
+Copyright (c) 2013 Zsombor Welker. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
